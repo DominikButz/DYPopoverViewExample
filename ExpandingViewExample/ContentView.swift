5aec0de485ab11eb65ed825994ba32919f654ed5
Dominik <dominikbutz@gmail.com>
2020-09-05 13:19:09 +0800

    example update for version 0.2

--- ExpandingViewExample/ContentView.swift
+++ ExpandingViewExample/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     @State var secondPopoverFrame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width * 0.75, height:150 )
 
     @State var viewId: String = ""
-    @State var popoverPosition: ViewPosition = .bottom
+    @State var popoverPosition: ViewPosition = .top
     
     var body: some View {
         NavigationView {
@@ -58,8 +58,8 @@ struct ContentView: View {
                 
                 
             }.frame(width: proxy.size.width).background(Color(.systemBackground))
-                .popoverView(content: AnyView(Text("Content")), isPresented: self.$showFirstPopover, frame: .constant(CGRect(x: 0, y: 0, width: 150, height: 150)), popoverType: .popout, position: self.popoverPosition, viewId: "0", settings: self.customPopoverSettings0())
-                .popoverView(content: AnyView(ContentExample(frame: self.$secondPopoverFrame, show:self.$showSecondPopover)), isPresented: self.$showSecondPopover, frame: self.$secondPopoverFrame, popoverType: .popover, position: self.popoverPosition, viewId: "1", settings: self.customPopoverSettings1())
+                .popoverView(content: {Text("Content")}, isPresented: self.$showFirstPopover, frame: .constant(CGRect(x: 0, y: 0, width: 150, height: 150)), popoverType: .popout, position: self.popoverPosition, viewId: "0", settings: self.customPopoverSettings0())
+                .popoverView(content: {ContentExample(frame: self.$secondPopoverFrame, show:self.$showSecondPopover)}, isPresented: self.$showSecondPopover, frame: self.$secondPopoverFrame, popoverType: .popover, position: self.popoverPosition, viewId: "1", settings: self.customPopoverSettings1())
 
          }
         }
