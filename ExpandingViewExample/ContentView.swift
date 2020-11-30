a2887a89f96cd08ba612c0105bac0820f949715a
Dominik <dominikbutz@gmail.com>
2020-11-30 11:30:55 +0800

    updated NavigationButton example.
    Popover now appears as expected if the  popover-modifier is attached to a GeometryReader which is the top-view in the body.

--- ExpandingViewExample/ContentView.swift
+++ ExpandingViewExample/ContentView.swift
@@ -11,17 +11,20 @@ import DYPopoverView
 
 struct ContentView: View {
     
-    @State var showSecondPopover = false
-    @State var showFirstPopover  = false
-    @State var showThirdPopover = false
-    @State var secondPopoverFrame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width * 0.75, height:150 )
+    @State private var showSecondPopover = false
+    @State private var showFirstPopover  = false
+    @State private var showThirdPopover = false
+    @State private var secondPopoverFrame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width * 0.75, height:150 )
 
-    @State var viewId: String = ""
-    @State var popoverPosition: ViewPosition = .top
+    @State private var viewId: String = ""
+    @State private  var popoverPosition: ViewPosition = .top
+    @State private var thirdPopoverBounds: CGRect?
+    
     
     var body: some View {
+        GeometryReader {  proxy in
         NavigationView {
-            GeometryReader {  proxy in
+            
                 VStack(alignment: .center) {
            
                 Spacer()
@@ -55,20 +58,16 @@ struct ContentView: View {
                 .frame(width: proxy.size.width).background(Color(.systemBackground))
                 .popoverView(content: {Text("Content")}, background: {BlurView(style: .systemChromeMaterial)}, isPresented: self.$showFirstPopover, frame: .constant(CGRect(x: 0, y: 0, width: 150, height: 150)), popoverType: .popout, position: self.popoverPosition, viewId: "0", settings: DYPopoverViewSettings(shadowRadius: 20))
                 .popoverView(content: {ContentExample(frame: self.$secondPopoverFrame, show:self.$showSecondPopover)}, background: {Color(.secondarySystemBackground)}, isPresented: self.$showSecondPopover, frame: self.$secondPopoverFrame, popoverType: .popover, position: self.popoverPosition, viewId: "1", settings: DYPopoverViewSettings(cornerRadius: (30, 30, 30, 30)))
-                .navigationBarItems(leading: self.navigationItem() )
+                .navigationBarItems(leading: Button("Pop"){self.showThirdPopover.toggle()}.anchorView(viewId: "3") )
                 .navigationTitle(Text("Test"))
 
+              //  .coordinateSpace(name: "popoverSpace")
+
          }
-        }
+        }.popoverView(content: { Text("Content")
+        }, background: { Color(.secondarySystemBackground) }, isPresented: self.$showThirdPopover, frame: .constant(CGRect(x:0, y:0, width: 200, height: 200)), popoverType: .popout, position: .bottomRight, viewId: "3")
     }
-    
-    func navigationItem()-> some View {
-        
-        Button("Pop"){ self.showThirdPopover.toggle()}.anchorView(viewId: "3")
-            .popoverView(content: { Text("Content")
-            }, background: { Color(.secondarySystemBackground) }, isPresented: self.$showThirdPopover, frame: .constant(CGRect(x:0, y:0, width: 200, height: 200)), popoverType: .popout, position: .bottomRight, viewId: "3")
-        
-    }
+
     
 //    func customPopoverSettings0()->DYPopoverViewSettings {
 //
@@ -87,9 +86,6 @@ struct ContentView: View {
 //        return settings
 //    }
 }
-
-
-//      return ExpandingView(content: AnyView(ContentView(isExpanded: self.isExpanded)), isExpanded: self.$isExpanded, originPreference: preferences.first!, expandedBounds: CGRect(x: 20, y: 20, width: UIScreen.main.bounds.width - 40, height: UIScreen.main.bounds.height - 40))
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
